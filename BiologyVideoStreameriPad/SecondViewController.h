961289ade52ef0b548525fa37194bc9c37fdc093
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2013-03-13 09:13:36 +0000

    Initial Commit

---
+++ BiologyVideoStreameriPad/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  BiologyVideoStreameriPad
+//
+//  Created by Joseph caxton-Idowu on 13/03/2013.
+//  Copyright (c) 2013 LearnersCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ BiologyVideoStreameriPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BiologyVideoStreameriPad
+//
+//  Created by Joseph caxton-Idowu on 13/03/2013.
+//  Copyright (c) 2013 LearnersCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
