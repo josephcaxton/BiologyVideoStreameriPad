96fa5dbe37559917219539199fcd8ef6a3898a58
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2013-03-13 09:47:19 +0000

    first commit

---
+++ Help.h
@@ -0,0 +1,26 @@
+//
+//  Help.h
+//  VideoStreamer
+//
+//  Created by Joseph caxton-Idowu on 13/03/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Help : UIViewController <UITableViewDataSource, UITableViewDelegate>{
+    
+    NSMutableArray *listofItems;
+    UIButton *LCButton;
+    UITableView *FirstTable;
+    CGRect FirstViewframe;
+}
+@property (nonatomic, retain) NSMutableArray *listofItems;
+@property (nonatomic, retain) UIButton *LCButton;
+@property (nonatomic, retain) UITableView *FirstTable;
+@property (nonatomic, assign)  CGRect FirstViewframe;
+
+- (void)WebsitebuttonPressed;
+
+
+@end
